1c0613afce344f04b951c4d3eb1cd64c99a8067d
Emanuel Sarco <emasar91@gmail.com>
2025-12-18 00:48:15 -0300

    fix navbar

--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { useEffect, useState } from 'react'
-import TabsNavbar from '@/components/Navbar_TMP/components/TabsNavbar/TabsNavbar'
+import TabsNavbar from '@/components/Navbar/components/TabsNavbar/TabsNavbar'
 import { Box, Link, Typography, useMediaQuery } from '@mui/material'
 import {
 	backBase,
