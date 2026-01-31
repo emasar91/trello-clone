d3d65f34c810a34f6db1ec5bdab9b227d46f1b8b
Emanuel Sarco <emasar91@gmail.com>
2026-01-31 02:45:37 -0300

    metadata img

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -34,7 +34,7 @@ export const metadata: Metadata = {
     siteName: "Trello Clone",
     images: [
       {
-        url: "/assets/metadata/trello.png",
+        url: "/trello.png",
         width: 1200,
         height: 630,
         alt: "Trello Clone",
